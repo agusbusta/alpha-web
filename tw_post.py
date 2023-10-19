358c66a0ffc7b81944eb4115e665751dff2ed271
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2023-10-19 10:02:54 -0300

    thread route working ok

--- tw_post.py
+++ tw_post.py
@@ -11,11 +11,39 @@ TWITTER_ACCESS_TOKEN=os.getenv("TWITTER_ACCESS_TOKEN")
 TWITTER_ACCESS_TOKEN_SECRET=os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 TWITTER_BEARER_TOKEN=os.getenv("TWITTER_BEARER_TOKEN")
 
-def send_tweets_to_twitter(string, content2):
+def split_text_into_paragraphs(text, max_length=270):
+    paragraphs = []
+    current_paragraph = ""
 
-    # paragraphs = ['', '']
+    for paragraph in text.split('\n'):
+        words = paragraph.split()
+        for word in words:
+            if len(current_paragraph) + len(word) + 1 <= max_length:
+                current_paragraph += word + " "
+            else:
+                paragraphs.append(current_paragraph.strip())
+                current_paragraph = word + " "
+        if current_paragraph:
+            paragraphs.append(current_paragraph.strip())
+        current_paragraph = ""
 
-    # Authenticate to Twitter
+    combined_paragraphs = []
+    current_paragraph = ""
+    for paragraph in paragraphs:
+        if len(current_paragraph) + len(paragraph) + 1 <= max_length:
+            if current_paragraph:
+                current_paragraph += " " + paragraph
+            else:
+                current_paragraph = paragraph
+        else:
+            combined_paragraphs.append(current_paragraph)
+            current_paragraph = paragraph
+    if current_paragraph:
+        combined_paragraphs.append(current_paragraph)
+
+    return combined_paragraphs
+
+def send_tweets_to_twitter(content):
     auth = tweepy.Client(
         TWITTER_BEARER_TOKEN,
         TWITTER_API_KEY,
@@ -24,82 +52,26 @@ def send_tweets_to_twitter(string, content2):
         TWITTER_ACCESS_TOKEN_SECRET
     )
     
-    response = auth.create_tweet(text=content1)
-    id = response[0].get("id")
+    paragraphs = split_text_into_paragraphs(content)
 
-    next_tweet = auth.create_tweet(text=content2, in_reply_to_tweet_id=id)
-    print('next_tweet > ', next_tweet)
+    if len(paragraphs) == 1:
+        # Si solo hay un párrafo, publica un solo tweet
+        response = auth.create_tweet(text=paragraphs[0])
+
+    else:
+        # Si hay varios párrafos, publica un hilo de tweets
+        id = None
+        for i, paragraph in enumerate(paragraphs):
+            if i == 0:
+                response = auth.create_tweet(text=paragraph)
+                id = response[0].get("id")
+            
+            else:
+                response = auth.create_tweet(text=paragraph, in_reply_to_tweet_id=id)
+              
     
     return 'ok', 200
 
 # Example usage
-content1 = "test1"
-content2 = "test1"
-send_tweets_to_twitter(content1, content2)
-
-
-
-##############
-# import tweepy
-
-# def send_tweets_to_twitter(content):
-    
-
-#     # Authenticate to Twitter
-#     auth = tweepy.Client(
-#         TWITTER_BEARER_TOKEN,
-#         TWITTER_API_KEY,
-#         TWITTER_KEY_SECRET,
-#         TWITTER_ACCESS_TOKEN,
-#         TWITTER_ACCESS_TOKEN_SECRET
-#     )
-
-#     try:
-#         # Validate input
-#         if not content or len(content) == 0:
-#             return "Error: Text is required and must have a length > 0", 400
-
-#         # Split content into paragraphs
-#         paragraphs = content.split(". ")
-#         print(paragraphs)
-        
-#         if len(paragraphs) == 1:
-#             # If there's only one paragraph, post a single tweet
-#             status = auth.create_tweet(text=paragraphs[0])
-#             print('1 tweet created and posted a')
-#             return "Success: Tweet posted successfully", 200
-#         else:
-#             # If there are multiple paragraphs, post as a thread
-#             tweet_thread = []
-
-#             for i, paragraph in enumerate(paragraphs):
-#                 # Ignore empty paragraphs
-#                 if not paragraph:
-#                     continue
-
-#                 tweet_text = paragraph
-
-#                 if i == 0:
-#                     # For the first paragraph, create a tweet
-#                     status = auth.create_tweet(text=tweet_text)
-#                     print(f"1 tweet created and posted b: {tweet_text}")
-#                 else:
-#                     # For subsequent paragraphs, reply to the previous tweet
-#                     status = auth.create_tweet(
-#                         text=tweet_text,
-#                         in_reply_to_status_id=tweet_thread[-1].id,
-#                         auto_populate_reply_metadata=True
-#                     )
-#                     print(f"Reply tweet created: {tweet_text}")
-                
-#                 tweet_thread.append(status)
-
-#             print('Thread posted successfully')
-#             return "Success: Thread posted successfully.", 200
-
-#     except Exception as e:
-#         return f"Error: {str(e)}", 500
-
-# Example usage
-# content = "This is paragraph 1. This is paragraph 2. This is paragraph 3. "
-# send_tweets_to_twitter(content)
+content = "Este es un ejemplo de un texto largo que queremos dividir en párrafos. Cada párrafo no debe exceder los 270 caracteres y debe tener puntos y aparte donde corresponda. Esto es un ejemplo de un párrafo más largo.Este es un ejemplo de un texto largo que queremos dividir en párrafos. Cada párrafo no debe exceder los 270 caracteres y debe tener puntos y aparte donde corresponda. Esto es un ejemplo de un párrafo más largo.."
+send_tweets_to_twitter(content)
