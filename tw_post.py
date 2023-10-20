a4a646a25207666475cd3e88ede6e5212e09cc18
daviddflix <david-972010@hotmail.com>
2023-10-20 12:28:49 -0300

    adding prints to tw_post

--- tw_post.py
+++ tw_post.py
@@ -1,7 +1,7 @@
-import re
 from tweepy.errors import TweepyException
 from dotenv import load_dotenv
 import tweepy
+import re
 import os
 
 load_dotenv()
@@ -20,8 +20,7 @@ auth = tweepy.Client(
     TWITTER_ACCESS_TOKEN_SECRET
 )
 
-print('Logged user >', auth.get_me()[0])
-
+# Split the summary into paragraphs of 280 characters max
 def split_string(input_string: str) -> str:
     result = []
 
@@ -30,54 +29,17 @@ def split_string(input_string: str) -> str:
     current_string = chunks[0]
     
     for part in chunks[1:]:
-        # Verificamos si el string actual junto con el próximo fragmento, incluyendo el dash
-        # tiene menos de 280 caracteres
         if len(current_string + "-" + part) < 280:
-            # Si es así, agregamos el fragmento y el dash al string actual
             current_string += "•" + part
         else:
-            # Si supera los 280 caracteres, agregamos el string actual a la lista de resultados
             result.append(current_string)
-            # Empezamos un nuevo string con el fragmento actual
             current_string = "•" + part
 
-    # Agregamos el último string al resultado
     result.append(current_string)
     
     return result
 
-def split_text_into_paragraphs(text, max_length=270):
-    paragraphs = []
-    current_paragraph = ""
-
-    for paragraph in text.split('\n'):
-        words = paragraph.split()
-        for word in words:
-            if len(current_paragraph) + len(word) + 1 <= max_length:
-                current_paragraph += word + " "
-            else:
-                paragraphs.append(current_paragraph.strip())
-                current_paragraph = word + " "
-        if current_paragraph:
-            paragraphs.append(current_paragraph.strip())
-        current_paragraph = ""
-
-    combined_paragraphs = []
-    current_paragraph = ""
-    for paragraph in paragraphs:
-        if len(current_paragraph) + len(paragraph) + 1 <= max_length:
-            if current_paragraph:
-                current_paragraph += " " + paragraph
-            else:
-                current_paragraph = paragraph
-        else:
-            combined_paragraphs.append(current_paragraph)
-            current_paragraph = paragraph
-    if current_paragraph:
-        combined_paragraphs.append(current_paragraph)
-
-    return combined_paragraphs
-
+# Finds the title in the summary
 def find_title_between_asterisks(text):
     match = re.search(r'\*(.*?)\*', text)
     if match:
@@ -86,6 +48,7 @@ def find_title_between_asterisks(text):
     else:
         return None
 
+# Makes the title in bold
 def bold(title):
     chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
     bold_chars = "𝗔𝗕𝗖𝗗𝗘𝗙𝗚𝗛𝗜𝗝𝗞𝗟𝗠𝗡𝗢𝗣𝗤𝗥𝗦𝗧𝗨𝗩𝗪𝗫𝗬𝗭𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇𝟬𝟭𝟮𝟯𝟰𝟱𝟲𝟳𝟴𝟵"
@@ -100,10 +63,11 @@ def bold(title):
     return output
 
 def send_tweets_to_twitter(content: str) -> list:
+
     if len(content) == 0:
         return 'Content has length 0', 404
 
-    paragraphs = split_string(content)
+    paragraphs = split_string(content) # separa el contenido en parrafos
     title = find_title_between_asterisks(paragraphs[0])  # Obtener el título original
     bold_title = bold(title)  # Obtener el título en negritas
     paragraphs[0] = title  # Reemplazar el título original en el primer párrafo
@@ -121,10 +85,13 @@ def send_tweets_to_twitter(content: str) -> list:
         try:
             for i, paragraph in enumerate(paragraphs):
                 if i == 0:
-                    response = auth.create_tweet(text=bold_title + '\n' + paragraphs[0])
-                    id = response[0].get("id")
+                    print('main tweet > ', bold_title + '\n' + paragraphs[0])
+                    # response = auth.create_tweet(text=bold_title + '\n' + paragraphs[0])
+                    # id = response[0].get("id")
                 else:
-                    response = auth.create_tweet(text=paragraph, in_reply_to_tweet_id=id)
+                    print('paragraph >', paragraph)
+                    # response = auth.create_tweet(text=paragraph, in_reply_to_tweet_id=id)
+                    # Here the id have to be replace too, for the next threds.
             return 'Summary sent to Twitter successfully', 200
         except TweepyException as e:
             print('An error occurred:' + str(e))
@@ -140,5 +107,45 @@ content = """
 - Some operators turning off unprofitable ATMs
 - Bitcoin Depot sees opportunity for market share growth through acquisitions and retail expansion.
 """
-# print(split_string(content)) # test the content
-send_tweets_to_twitter(content) # test sending the tweet
+
+send_tweets_to_twitter(content) # Test sending the tweet
+
+
+
+
+
+
+
+
+# def split_text_into_paragraphs(text, max_length=270):
+
+#     paragraphs = []
+#     current_paragraph = ""
+
+#     for paragraph in text.split('\n'):
+#         words = paragraph.split()
+#         for word in words:
+#             if len(current_paragraph) + len(word) + 1 <= max_length:
+#                 current_paragraph += word + " "
+#             else:
+#                 paragraphs.append(current_paragraph.strip())
+#                 current_paragraph = word + " "
+#         if current_paragraph:
+#             paragraphs.append(current_paragraph.strip())
+#         current_paragraph = ""
+
+#     combined_paragraphs = []
+#     current_paragraph = ""
+#     for paragraph in paragraphs:
+#         if len(current_paragraph) + len(paragraph) + 1 <= max_length:
+#             if current_paragraph:
+#                 current_paragraph += " " + paragraph
+#             else:
+#                 current_paragraph = paragraph
+#         else:
+#             combined_paragraphs.append(current_paragraph)
+#             current_paragraph = paragraph
+#     if current_paragraph:
+#         combined_paragraphs.append(current_paragraph)
+
+#     return combined_paragraphs
