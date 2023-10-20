5a628cf1814c100a48675d80dbb75f97a498a274
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2023-10-20 10:44:58 -0300

    test twitter 1

--- tw_post.py
+++ tw_post.py
@@ -1,3 +1,4 @@
+import re
 from tweepy.errors import TweepyException
 from dotenv import load_dotenv
 import tweepy
@@ -5,23 +6,21 @@ import os
 
 load_dotenv()
 
-
-TWITTER_API_KEY=os.getenv("TWITTER_API_KEY")
-TWITTER_KEY_SECRET=os.getenv("TWITTER_KEY_SECRET")
-TWITTER_KEY_SECRET=os.getenv("TWITTER_KEY_SECRET")
-TWITTER_ACCESS_TOKEN=os.getenv("TWITTER_ACCESS_TOKEN")
-TWITTER_ACCESS_TOKEN_SECRET=os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
-TWITTER_BEARER_TOKEN=os.getenv("TWITTER_BEARER_TOKEN")
+TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
+TWITTER_KEY_SECRET = os.getenv("TWITTER_KEY_SECRET")
+TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
+TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+TWITTER_BEARER_TOKEN = os.getenv("TWITTER_BEARER_TOKEN")
 
 auth = tweepy.Client(
-        TWITTER_BEARER_TOKEN,
-        TWITTER_API_KEY,
-        TWITTER_KEY_SECRET,
-        TWITTER_ACCESS_TOKEN,
-        TWITTER_ACCESS_TOKEN_SECRET
-    )
+    TWITTER_BEARER_TOKEN,
+    TWITTER_API_KEY,
+    TWITTER_KEY_SECRET,
+    TWITTER_ACCESS_TOKEN,
+    TWITTER_ACCESS_TOKEN_SECRET
+)
 
-print('Logged user > ', auth.get_me()[0])
+print('Logged user >', auth.get_me()[0])
 
 def split_string(input_string: str) -> str:
     result = []
@@ -79,48 +78,60 @@ def split_text_into_paragraphs(text, max_length=270):
 
     return combined_paragraphs
 
-def send_tweets_to_twitter(content: str) -> list:
+def find_title_between_asterisks(text):
+    match = re.search(r'\*(.*?)\*', text)
+    if match:
+        title = match.group(1)
+        return title
+    else:
+        return None
 
+def bold(title):
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    bold_chars = "𝗔𝗕𝗖𝗗𝗘𝗙𝗚𝗛𝗜𝗝𝗞𝗟𝗠𝗡𝗢𝗣𝗤𝗥𝗦𝗧𝗨𝗩𝗪𝗫𝗬𝗭𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇𝟬𝟭𝟮𝟯𝟰𝟱𝟲𝟳𝟴𝟵"
+
+    output = ""
+
+    for character in title:
+        if character in chars:
+            output += bold_chars[chars.index(character)]
+        else:
+            output += character
+    return output
+
+def send_tweets_to_twitter(content: str) -> list:
     if len(content) == 0:
         return 'Content has length 0', 404
-   
+
     paragraphs = split_string(content)
+    bold_title = bold(find_title_between_asterisks(paragraphs[0]))  # Obtener título en negritas
+    paragraphs[0] = find_title_between_asterisks(paragraphs[0])  # Eliminar ** del título
 
     if len(paragraphs) == 1:
         try:
-
             # Si solo hay un párrafo, publica un solo tweet
-            response = auth.create_tweet(text=paragraphs[0])
-            return 'Summary sent to twitter successfully', 200
-        
+            response = auth.create_tweet(text=bold_title + '\n' + paragraphs[0])
+            return 'Summary sent to Twitter successfully', 200
         except TweepyException as e:
-            print('An error occured:' + str(e))
-            return 'An error occured:' + str(e), 500
-
+            print('An error occurred:' + str(e))
+            return 'An error occurred:' + str(e), 500
     else:
-
-        # Si hay varios párrafos, publica un hilo de tweets
         id = None
         try:
-
             for i, paragraph in enumerate(paragraphs):
                 if i == 0:
-                    response = auth.create_tweet(text=paragraph)
+                    response = auth.create_tweet(text=bold_title + '\n' + paragraphs[0])
                     id = response[0].get("id")
                 else:
                     response = auth.create_tweet(text=paragraph, in_reply_to_tweet_id=id)
-            return 'Summary sent to twitter successfully', 200
-        
+            return 'Summary sent to Twitter successfully', 200
         except TweepyException as e:
-            print('An error occured:' + str(e))
-            return 'An error occured:' + str(e), 500
-              
-    
-
+            print('An error occurred:' + str(e))
+            return 'An error occurred:' + str(e), 500
 
 # Example usage
 content = """
-Bitcoin ATM installations hit two-year low worldwide
+*Bitcoin ATM installations hit two-year low worldwide*
 - Number of bitcoin ATMs dropped by 17%
 - US experienced the biggest decline, now has 26,700 machines
 - Europe has only 1,500 machines
@@ -129,4 +140,4 @@ Bitcoin ATM installations hit two-year low worldwide
 - Bitcoin Depot sees opportunity for market share growth through acquisitions and retail expansion.
 """
 # print(split_string(content)) # test the content
-# send_tweets_to_twitter(content) # test sending the tweet
+send_tweets_to_twitter(content) # test sending the tweet
